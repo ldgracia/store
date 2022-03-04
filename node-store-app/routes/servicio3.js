a508f292bec68e7c23c837bd5e7a42821e20d1a7
LeonardoRosalesG <lrosalesg2399@gmail.com>
2022-03-04 14:21:35 -0600

    add:servicio3

---
+++ node-store-app/routes/servicio3.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/servicio3', function(req, res, next) {
+  res.send("hola desde servicio3");
+});
+
+
+module.exports = router;
