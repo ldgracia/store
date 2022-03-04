a1d3ebdce5d98e6d3387a94da2698dd3da064144
LeonardoRosalesG <lrosalesg2399@gmail.com>
2022-03-04 15:15:49 -0600

    modify:servicio3

--- node-store-app/routes/servicio3.js
+++ node-store-app/routes/servicio3.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/servicio3', function(req, res, next) {
-  res.send("hola desde servicio3");
+  res.send("hola desde servicio3:)");
 });
 
 
