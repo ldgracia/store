2b812cc451e35dd05b8c8981612b9a7eb6c7b3a1
gustavo96t <s.gustavo96@gmail.com>
2022-03-04 15:55:45 -0600

    servicio6

--- node-store-app/app.js
+++ node-store-app/app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 
-var servicio1Router = require('./routes/servicio1');
+var servicio6Router = require('./routes/servicio6');
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/servicio1', servicio1Router);
+app.use('/servicio6', servicio6Router);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
