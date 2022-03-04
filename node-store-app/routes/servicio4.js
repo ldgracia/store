2299b2957be3e0011bb5b2ae1964887b81ca0cef
Fdo Hrdz E <fhernandeze1600@alumno.ipn.mx>
2022-03-04 15:45:04 -0600

    actualizando nombre

--- node-store-app/routes/servicio4.js
+++ node-store-app/routes/servicio4.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/servicio4', function(req, res, next) {
-  res.send("hola desde servicio4");
+  res.send("hola desde servicio_4");
 });
 
 
